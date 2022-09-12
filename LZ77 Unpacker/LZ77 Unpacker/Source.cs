79541ed804d9083b135fa5ce7b35b8f42f0d3758
Edshiro <hot.hunter@mail.ru>
2022-09-12 22:03:37 +0600

    22:04 - 12.09.2022

---
+++ LZ77 Unpacker/LZ77 Unpacker/Source.cs
@@ -0,0 +1,30 @@
+﻿namespace LZ77_Unpacker
+{
+    public class LZ77Unpacked
+    {
+        public static void Unpack(string path)
+        {
+            byte[] file = File.ReadAllBytes(path);
+            using StreamWriter fileOutput = new (path + ".txt");
+            var W = Math.Pow(2, 16);
+            string w = "";
+            for (int i = 0; i < W; i++) w += "=";
+            for(int i = 0; i<file.Length; i+=4)
+            {
+                int pos = 0xFFFF & ((Convert.ToInt32(file[i]) << 8) ^ (Convert.ToInt32(file[i+1])));
+                int len = 0xFF & (Convert.ToInt32(file[i+2]));
+                string c = Convert.ToChar(file[i + 3]).ToString();
+
+                fileOutput.Write(string.Concat(w.AsSpan(pos, len), c));
+                
+                w = string.Concat(w[(len + 1)..], w.AsSpan(pos, len), c);
+
+            }
+            fileOutput.Close();
+        }
+
+        public static void Main(string[] args) => Unpack("file.lz77");
+    }
+
+
+}
