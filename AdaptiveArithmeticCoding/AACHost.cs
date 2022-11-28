7f50a0cc1d7de78e6ab2ce868d71b201fdea400f
Rodion Pilipenko <49054722+RodKingroo@users.noreply.github.com>
2022-11-28 20:47:41 +0600

    20:47 - 28.11.2022

--- AdaptiveArithmeticCoding/AACHost.cs
+++ AdaptiveArithmeticCoding/AACHost.cs
@@ -7,16 +7,16 @@ namespace AAC
         /// <summary> 
         /// Входные данные, прописанные в задании
         /// </summary>
-        private const double input = 0.5790710929829856;
+        private const double input = 759021.0/4194304.0;//0.5790710929829856;
 
         /// <summary> 
         /// Список всех символов
         /// </summary>
         private List<string> letters = new()
         {
-            "О", "Е", "А", "И", "Н", "Т", "С", "Р", "В", "Л", "К", 
-            "М", "Д", "П", "У", "Я", "З", "Ы", "Б", "Г", "Ч", "Й", 
-            "Х", "Ж", "Ъ", "Ь", "Ш", "Ю", "Ц", "Щ", "Э", "Ф", " "
+           " ", "C", "B", "A" // "О", "Е", "А", "И", "Н", "Т", "С", "Р", "В", "Л", "К", 
+           // "М", "Д", "П", "У", "Я", "З", "Ы", "Б", "Г", "Ч", "Й", 
+           // "Х", "Ж", "Ъ", "Ь", "Ш", "Ю", "Ц", "Щ", "Э", "Ф", " "
         };
 
         /// <summary> 
@@ -24,9 +24,9 @@ namespace AAC
         /// </summary>
         private List<double> size = new()
         {
-            90, 72, 62, 62, 53, 53, 45, 40, 38, 35, 28, 
-            26, 25, 23, 21, 18, 16, 16, 14, 13, 12, 10, 
-            9,  7,  7,  7,  6,  6,  4,  3,  3,  1,  1
+            1, 1, 1, 1 //90, 72, 62, 62, 53, 53, 45, 40, 38, 35, 28, 
+            //26, 25, 23, 21, 18, 16, 16, 14, 13, 12, 10, 
+            //9,  7,  7,  7,  6,  6,  4,  3,  3,  1,  1
         };
 
         /// <summary> 
@@ -43,25 +43,27 @@ namespace AAC
         private List<double> _dictinarySizeB = new(){1};
         private List<string> _word = new();
 
-        protected void findfirstletter(List<double> a, List<double> b, 
+        protected void findletter(List<double> a, List<double> b, 
                                        List<double> size, double asz)
         {
             Console.WriteLine(asz);
-            for(int i = 0; i < size.Count; i++)
+            for(int i = 1; i < size.Count+1; i++)
             {
-                double btoa = a[0] + size[size.Count - i - 1] * (b[i] - a[0]) / asz;
-                b.Insert(0, btoa);
-                a.Insert(0, b[0]);
-                Console.WriteLine($"{a[1]} - {b[0]}");
+                double btoa = a[i-1] + size[size.Count - i] * (b[0] - a[0]) / asz;
+                b.Add(btoa);
+                a.Add(b[i]);
+                Console.WriteLine($"{a[i-1]} - {b[i]}");
             }
 
-            for(int j = 0; j<size.Count; j++) 
+            for(int j = 1; j<=size.Count; j++) 
             {
-                if(a[j+1] < input && b[j] > input) { 
-                    _word.Add(letters[j]);
+                //Console.WriteLine($"[ {a[j-1]} ]-[ {b[j]} ]");
+                if(a[j-1] < input && b[j] > input) { 
+                    _word.Add(letters[size.Count - j]);
                     newValue(a, j, size, b, asz);
                     break;
                 }
+                
             }
 
 
@@ -72,23 +74,25 @@ namespace AAC
 
         protected void newValue(List<double> a, int j, List<double> size, List<double> b, double asize)
         {
-            a.RemoveAll(item => item != a[j+1]);
+            a.RemoveAll(item => item != a[j-1]);
             b.RemoveAll(item => item != b[j]);
-            size.RemoveAt(j);
-            size.Insert(j, size[j] + 1);
+            //size.RemoveAt(size.Count - j);
+            //size.Insert(size.Count - j, size[size.Count - j] + 1);
+            size[j - 1] += 1;
             asize = asize + 1;
-            findfirstletter(a, b, size, asize);
+            findletter(a, b, size, asize);
             //Console.WriteLine(size[j]);
+            //Console.WriteLine($"Новая граница А: { string.Join("", a) } Новая граница B: { string.Join("", b) } {size[size.Count - j]} {asize}");
             
         }
 
         public void Run()
         {
-            /*Console.WriteLine($"Входное значение: {input}");
-            for(int i = 0; i < size.Count; i++) Console.WriteLine($"[ {letters[i]} ] - [ {size[i]} ]");
+            Console.WriteLine($"Входное значение: {input}");
+            /*for(int i = 0; i < size.Count; i++) Console.WriteLine($"[ {letters[i]} ] - [ {size[i]} ]");
             Console.WriteLine($"Общий вес: {sizeletters(_asize)}");*/
             double asize = sizeletters(_asize);
-            findfirstletter(_dictinarySizeA, _dictinarySizeB, size, asize);
+            findletter(_dictinarySizeA, _dictinarySizeB, size, asize);
         }
 
     }
