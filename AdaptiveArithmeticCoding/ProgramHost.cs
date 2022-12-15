977e4c2fe9c0d563a36993db9735c77cf5a911c5
Rodion Pilipenko <49054722+RodKingroo@users.noreply.github.com>
2022-12-15 09:42:17 +0600

    9:41 - 15.12.2022

--- AdaptiveArithmeticCoding/ProgramHost.cs
+++ AdaptiveArithmeticCoding/ProgramHost.cs
@@ -2,97 +2,77 @@ namespace AAC
 {
     public class ProgramHost 
     {
-        private double _edgeA = 0, _edgeB = 1;
-
-        /// <summary> 
-        /// Входные данные, прописанные в задании
-        /// </summary>
-        private const double input = 0.5790710929829856; //759021.0/4194304.0;
-
-        /// <summary> 
-        /// Список всех символов
-        /// </summary>
+        private double edgeLower = 0;
+        private double edgeUpper = 1;
+        private const double input = 0.5790710929829856; 
         private List<string> letters = new()
         {
-           // "A", "B", "C", " " 
             "О", "Е", "А", "И", "Н", "Т", "С", "Р", "В", "Л", "К", 
             "М", "Д", "П", "У", "Я", "З", "Ы", "Б", "Г", "Ч", "Й", 
             "Х", "Ж", "Ъ", "Ь", "Ш", "Ю", "Ц", "Щ", "Э", "Ф", " "
         };
 
-        /// <summary> 
-        /// Список веса всех символов
-        /// </summary>
         private List<double> size = new()
         {
-            //1, 1, 1, 1 
             90, 72, 62, 62, 53, 53, 45, 40, 38, 35, 28, 
             26, 25, 23, 21, 18, 16, 16, 14, 13, 12, 10, 
             9,  7,  7,  7,  6,  6,  4,  3,  3,  1,  1
         };
 
-        /// <summary> 
-        /// Количество всех символов в "библиотеке"
-        /// </summary>
-        private double _asize = 0;
-        internal double sizeletters(double asize)
+        private double aSize = 0;
+        protected double sizeletters(double asize)
         {
-            for(int i = 0; i<size.Count; i++) asize += size[i];
+            asize = size.Sum();
             return asize;
         }
 
-        private List<double> _dictinarySizeA = new(){};
-        private List<double> _dictinarySizeB = new(){};
-        private List<string> _word = new();
+        private List<double> dictinarySizeRight = new (){};
+        private List<string> word = new();
 
-        protected void findLetter(List<double> a, List<double> b, 
-                                  List<double> size, double asz, 
-                                  double _edgeA, double _edgeB)
+        protected void findLetter(List<double> right, List<double> size, 
+                                  double asz, double edgeLower, double edgeUpper)
         {
-            a.Add(_edgeA);
+            int countSize = size.Count - 1;
+            right.Add(edgeLower);
             for(int i = 0; i < size.Count; i++)
             {
-                double btoa = a[i] + size[size.Count - (i + 1)] * (_edgeB - _edgeA) / asz;
-                b.Add(btoa);
-                a.Add(b[i]);
-                //Console.WriteLine($"{a[i]} - {b[i]}");
+                var btoa = right[i] + size[countSize - i] * (edgeUpper - edgeLower) / asz;
+                right.Add(btoa);
             }
 
-            b.Reverse();
-            a.Reverse();
+            int countRight = right.Count-1;            
 
-            for(int j = 0; j <= size.Count - 1; j++)
+            for(int j = 0; j < countRight; j++)
             {
-                Console.WriteLine($"{a[j + 1]} - {b[j]}");
-                if(a[j + 1] < input && b[j] > input)
+                if(right[countRight-(j+1)] < input && right[countRight-j] > input)
                 {
-                    _word.Add(letters[j]);
-                    newValue(a, b, a[j + 1], b[j], j, size, asz);
+                    word.Add(letters[j]);
+                    if(word[word.Count-1] == " ") break;
+                    newValue(right, right[countRight-(j+1)], right[countRight-j], j, size, asz);
                     break;
                 }
             }
 
-            Console.WriteLine(string.Join("", _word));
+            
             
         }
 
-        protected void newValue(List<double> a, List<double> b,
-                                double _a, double _b,  int i, 
+        protected void newValue(List<double> Right,
+                                double left, double right,  int i, 
                                 List<double> size, double asize)
         {
-            a.Clear();
-            b.Clear();
+            Right.Clear();
             size[i] += 1;
             asize += 1;
-            findLetter(a, b, size, asize, _a, _b);
+            findLetter(Right, size, asize, left, right);
         }
 
         public void Run()
         {
             Console.WriteLine($"Входное значение: {input}");
-            double asize = sizeletters(_asize);
-            Console.WriteLine($"Общий вес: {asize}");
-            findLetter(_dictinarySizeA, _dictinarySizeB, size, asize, _edgeA, _edgeB);
+            double asize = sizeletters(aSize);
+            findLetter(dictinarySizeRight, size, asize, edgeLower, edgeUpper);
+            Console.WriteLine(string.Join("", word));
         }
         
     }
